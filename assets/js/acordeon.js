48113d98d1a239ecdfbc76ca8216b51093d4b396
Amadeu Jose Andrade Junior <amadeujajunior@gmail.com>
2024-07-22 14:40:31 -0300

    adiçao coisas

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,14 +1,18 @@
-var acc = document.getElementsByClassName("acordeon");
+var acc = document.getElementsByClassName('acordeon');
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+    acc[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        let button = this.firstElementChild;
+        let toggleSpan = button.lastElementChild;
+        let toggle = toggleSpan.firstElementChild;
+        toggle.classList.toggle('rotation');
+        var panel = this.lastElementChild;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    });
 }
